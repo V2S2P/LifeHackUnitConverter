9c116786b02170a03b25c4c9b1ccf4752fab3a27
V2S2P <157969468+V2S2P@users.noreply.github.com>
2025-04-11 23:54:23 +0200

    app.jar file (fat jar)

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -12,16 +12,15 @@ import java.util.logging.Logger;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
-/*
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
-    private static final String DB = "lifehackspring2025";
+    private static final String DB = "Converter";
 
 
     private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
 
- */
+
 
     public static void main(String[] args) {
 
